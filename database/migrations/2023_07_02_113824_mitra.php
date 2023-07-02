a24bcedc9ffac616dd9a6470d6945bcb662fac90
hiragisatou <yae.gyakushinnmiko@gmail.com>
2023-07-02 20:15:55 +0800

    update database

---
+++ database/migrations/2023_07_02_113824_mitra.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mitra', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->foreignId('kriteriaMitra_id');
+            $table->string('nomorIndukBerusaha');
+            $table->foreignId('sektorIndustri_id');
+            $table->foreignId('sifat_id');
+            $table->foreignId('jenis_id');
+            $table->string('klasifikasi');
+            $table->string('jumlahPegawai');
+            $table->string('alamat');
+            $table->string('provinsi');
+            $table->string('kabupaten');
+            $table->string('kecamatan');
+            $table->string('urlWeb')->nullable();
+            $table->string('email')->nullable();
+            $table->string('noTelp')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('tiktok')->nullable();
+            $table->string('youtube')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mitra');
+    }
+};
---
+++ database/migrations/2023_07_02_114822_pengajuan_kemitraan.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengajuanKemitraan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mitra_id');
+            $table->string('jenisKemitraan');
+            $table->string('ruangLingkup');
+            $table->dateTime('tgl_mulai');
+            $table->dateTime('tgl_berakhir');
+            $table->foreignId('prodi_id');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengajuanKemitraan');
+    }
+};
